cc4af4946b49a6300133ab4afb45e73ef03c2737
Trinithea <mysakmazplus@gmail.com>
2025-03-26 19:55:28 +0100

    Propadávací piškovrky kód daleko jasněji

--- 01_KODY_Z_HODIN/PropadavaciPiskvorky/PropadavaciPiskvorky/Program.cs
+++ 01_KODY_Z_HODIN/PropadavaciPiskvorky/PropadavaciPiskvorky/Program.cs
@@ -4,261 +4,114 @@ using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using namespace; -> díky tomu můžeme propojovat projekty mezi sebou
 
+namespace PropadavaciPiskvorky
+{   
 
-// namespace - jmenný prostor, ve kterém definujeme třídy
-namespace ConnectGame
-{   // složené závorky vymezují určité bloky kódu, omezují viditelnost a existenci tříd, funkcí, proměnných, ...
-
-
-    // internal - tato třída je viditelná všude ve výsledném EXE souboru (tzv. assembly)
-    // MODIFIKÁTORY PŘÍSTUPU (ACCESSIBILITY MODIFIERS)
-    // public, private, protected, ... internal
-        // public - viditelné všude
-        // private - viditelné jen v dané třídě - defaultní nastavení (pokud není uvedeno, tak private)
-    // -> ZAPOUZDŘENÍ (viditelnost určitých částí kódu)
-
-    internal class Program // v C# je vše ve třídě, v třídě jsou vlasnotsi, funkce a datové položky
+    internal class Program 
     {
         static void Main(string[] args)
         {
-            // C# notace pro pojmenování
-            // třídy, funkce, metody, veřejné vlastnosti: PascalCase notace
-            // privátní a lokální proměnné:  camelCasel notace
-            // v Pythonu: snake_case, v C# ne
+            // Main chceme mít co nejstručnější
 
-            int i = 0;
+            // zeptáme se hráče na parametry pro hru (inspirujte se rozšířeními)
 
-            Console.WriteLine(); // statické funkce lze volat ppouze bez instance
-            A a = new A();
-            a.Ahoj(); // nestatické pouze na instanci
-                      // A.Ahoj();
+            // podle toho vytvoříme hru
+            Hra hra = new Hra(5,6,7,2); // voláme konstruktor při vytváření instance
+            Hrac vitez = hra.Play(); // funkce Play vrací vítěze
 
-
-            int[,] board = new int[6, 7]{
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 1, 0, 0, 0 },
-                { 0, 0, 0, 1, 0, 0, 0 },
-                { 0, 0, 0, 1, 2, 1, 0 },
-                { 2, 2, 2, 2, 2, 1, 0 }
-            };
-            int[] position = { 5, 0 };
-            // pozice řádek s indexem 5
-            // pozice sloupec s indexem 0
-
-            int hrac = 2;
-            int pocetKamenuNaVyhru = 5;
-
-            Hra hra1 = new Hra(5, 6, 7,2);
-            // Hra hra2 = new Hra();
-
-
-            hra1.Play();
-
-
-            Console.WriteLine(hra1.Check(board, position, hrac, pocetKamenuNaVyhru));
-            Console.ReadLine();
-
+            // vypište vítěze
         }
 
     }
 
     class Hra
     {
-        //konstruktor
-        public Hra(int pocetVyhernichZetonu, int sirkaPole, int vyskaPole, int pocetHracu)
+        //konstruktor - jeho parametry by měly obsahovat vše, co je potřeba nastavit při inicializaci herního objektu (jedné instanci hry)
+        public Hra(int pocetVyhernichZetonu, int pocetSloupcu, int pocetRadku, int pocetHracu)
         {
-            this.pocetVyhernichZetonu = pocetVyhernichZetonu;
-            hraciPole = new int[sirkaPole, vyskaPole];
-            hraci = new Hrac[pocetHracu];
+            this.pocetVyhernichZetonu = pocetVyhernichZetonu; // použili jsme slovo this, protože se parametr konstruktoru nazývá stejně jako datová položka v této třídě
+            hraciPole = new int[pocetSloupcu, pocetRadku];
+
+            hraci = new Hrac[pocetHracu]; // vytvoří pole ale už nevytvoří samotné hráče! zatím jsou v poli jen samé null hodnoty
+            VytvorHrace(); // hráči v poli se vytvoří až v této funkci
+
         }
 
-        int pocetVyhernichZetonu; // datová položka
-        int[,] hraciPole;
+        // následující datové položky jsou privátní - není potřeba, aby byly vidět mimo třídu
+        private int pocetVyhernichZetonu; // datová položka
+        private int[,] hraciPole;
+        private Hrac[] hraci;
 
-        Hrac[] hraci;
+        private void VytvorHrace()
+        {
+            for (int i = 0; i < hraci.Length; i++)
+            {
+                Console.WriteLine($"Napiš jméno hráče {i}:");
+                string jmeno = Console.ReadLine();
+                Console.WriteLine($"Jaký symbol pro něj budeš chtít použít:");
+                char symbol = Convert.ToChar(Console.ReadLine());
+
+                hraci[i] = new Hrac(jmeno, symbol); // až zde dojde k reálnému vytvoření hráče v paměti (konkrétně na *haldě*)
+            }
+        }
+
+        // hru ovšem musíme nějak spustit, je tedy public, aby šla spustit i ze třídy Program, vrací vítězného hráče
         public Hrac Play()
         {
-            Hrac hrac = new Hrac(); 
-            Position startPosition = new Position();
-            Console.WriteLine("Na řadě je hráč", hrac.Jmeno);
-            startPosition.Row = 0;
-            startPosition.Column = 0;
+            // TODO - následující úkoly si zaslouží vlastní funkci/metodu
 
-
-            // Tah
-            // Check
-            //střídání hračů
+            // dokud hra neskončí
+                // vypište hráče, který je na řadě
+                // vypište herní pole
+                // zpracujte hráčův tah (např. zeptejte se do jakého sloupce chce vhodit žeton a podle toho na správné místo v hracím poli umístěte jeho žeton)
+                // zkontrolujte, zda tímto tahem nevyhrál
+                // pokud ne, je na řadě další hráč
+                // pokud ano, vraťce vítěze
         }
-        public bool Check(int[,] board, int[] soucasnaPozice, int hrac, int pocetKamenuNaVyhru)
+        private bool Check(Position soucasnaPozice, Hrac hrac)
         {
-            int radek = soucasnaPozice[0];
-            int sloupec = soucasnaPozice[1];
-            return CheckColumn(board, radek, sloupec, hrac, pocetKamenuNaVyhru) || CheckRow(board, radek, sloupec, hrac, pocetKamenuNaVyhru) || CheckDiag(board, radek, sloupec, hrac, pocetKamenuNaVyhru);
+            return CheckColumn(soucasnaPozice, hrac) || CheckRow(soucasnaPozice, hrac) || CheckDiag(soucasnaPozice, hrac);
         }
 
-        bool CheckColumn(int[,] gameField, int radek, int sloupec, int currentPlayer, int reqForWin)
+        private bool CheckColumn(Position soucasnaPozice, Hrac hrac)
         {
-            int pocet = 0;
-            int pocetRadku = gameField.GetLength(0);
-
-            for (int i = radek; i < pocetRadku; i++) 
-            {
-                if (gameField[i, sloupec] == currentPlayer)
-                {
-                    pocet++;
-                    if (pocet >= reqForWin) 
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-            return false;
+            return true; 
         }
 
-        bool CheckRow(int[,] gameField, int radek, int sloupec, int currentPlayer, int reqForWin)
+        private bool CheckRow(Position soucasnaPozice, Hrac hrac)
         {
-            int pocet = 1; 
-            int pocetSloupcu = gameField.GetLength(1);
-
-            for (int i = sloupec + 1; i < pocetSloupcu; i++)
-            {
-                if (gameField[radek, i] == currentPlayer)
-                {
-                    pocet++;
-                    if (pocet >= reqForWin)
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            
-            for (int i = sloupec - 1; i >= 0; i--) 
-            {
-                if (gameField[radek, i] == currentPlayer)
-                {
-                    pocet++;
-                    if (pocet >= reqForWin)
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            return false;
+            return true;
         }
 
-        bool CheckDiag(int[,] gameField, int radek, int sloupec, int currentPlayer, int reqForWin)
+        private bool CheckDiag(Position soucasnaPozice, Hrac hrac)
         {
-            int pocet = 1;
-            int pocetRadku = gameField.GetLength(0);
-            int pocetSloupcu = gameField.GetLength(1);
-
-            for (int i = 1; radek + i < pocetRadku && sloupec + i < pocetSloupcu; i++) 
-            {
-                if (gameField[radek + i, sloupec + i] == currentPlayer)
-                {
-                    pocet++;
-                    if (pocet >= reqForWin)
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            for (int i = 1; radek - i >= 0 && sloupec - i >= 0; i++) 
-            {
-                if (gameField[radek - i, sloupec - i] == currentPlayer)
-                {
-                    pocet++;
-                    if (pocet >= reqForWin)
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-
-            pocet = 1; 
-
-
-            for (int i = 1; radek - i >= 0 && sloupec + i < pocetSloupcu; i++) 
-            {
-                if (gameField[radek - i, sloupec + i] == currentPlayer)
-                {
-                    pocet++;
-                    if (pocet >= reqForWin)
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            for (int i = 1; radek + i < pocetRadku && sloupec - i >= 0; i++) 
-            {
-                if (gameField[radek + i, sloupec - i] == currentPlayer)
-                {
-                    pocet++;
-                    if (pocet >= reqForWin)
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            return false;
+            return true;
         }
 
     }
 
     class Hrac
     {
-        public string Jmeno;
-        public string Symbol;
+        // Jméno i Symbol budeme využívat i v třídě Hra, proto jsou PUBLIC
+        // Protože jsou public, tak je píšeme s velkým počátečním písmenem
+        public string Jmeno { get; } 
+        public char Symbol { get; }
+
+        public Hrac(string jmeno, char symbol)
+        {
+            Jmeno = jmeno;
+            Symbol = symbol;
+        }
     }
-    struct Position
+
+    // struktura je jako třída, ale paměťově omezená, hodí se pro jednoduché datové typy jako například udržování dvojice souřadnic pro pozici ve hře
+    struct Position 
     {
         public int Row;
         public int Column;
     }
 
 
-    class A
-    {
-        public void Ahoj()
-        {
-
-        }
-    }
+    
 }
